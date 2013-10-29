3db393599e47c812b3dc7535704e19ea0e0f171e
Alexander Wahl-Rasmussen <a.wahl.r@gmail.com>
2013-10-29 12:24:23 +0100

    Image split

--- imgrank.py
+++ imgrank.py
@@ -1,28 +1,48 @@
 """ Dem images zomg """
 from sklearn.datasets import fetch_lfw_people as LFW
 from skimage.filter import threshold_otsu
-import pylab
+from PIL import Image
+import glob
+import matplotlib.pyplot as pl
 import numpy as np
 
+# read image to array and convert to grayscale
+files = glob.glob("Images/*")
+
+grayimg=[]
+for img in files:
+    im = np.array(Image.open(img).convert('L'))
+    grayimg.append(im)
+print len(grayimg)
+   
 #Variables needed
 i = 0
 
 
-pylab.gray()
 ims = LFW(min_faces_per_person=20)
-print type(ims.images)
 grayfaces = ims.images[:8]
-print grayfaces
-grayfaces.sort()
-print grayfaces
 
+
+
+#Printing the pictures
+pl.gray()
 
 for im in grayfaces:
 	#We use this to sort the images by their mean
-	np.mean(im.flatten())
+	#np.mean(im.flatten())
 
-	pylab.subplot(241+i)
+	pl.subplot(241+i)
 	i += 1
-	pylab.imshow(im)
-pylab.show()
+	pl.imshow(im)
+	pl.axis('off')
 
+pl.show()
+
+s=0
+print len(grayimg)
+for gim in grayimg:
+	pl.subplot(241+s)
+	s+=1
+	pl.imshow(gim)
+	
+pl.show()
