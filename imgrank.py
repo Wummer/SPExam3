39dca346e457913bcf3776c6f6a4e4295c452498
Alexander Wahl-Rasmussen <a.wahl.r@gmail.com>
2013-10-29 12:38:40 +0100

    Loading the 2 datasets + printing them - Done.

--- imgrank.py
+++ imgrank.py
@@ -7,30 +7,29 @@ import matplotlib.pyplot as pl
 import numpy as np
 
 # read image to array and convert to grayscale
-files = glob.glob("Images/*")
+files = glob.glob("data/*/*.jpg")
 
-grayimg=[]
-for img in files:
-    im = np.array(Image.open(img).convert('L'))
-    grayimg.append(im)
-print len(grayimg)
-   
-#Variables needed
+grayimg1=[]
+grayimg2=[]
 i = 0
+s = 0
 
+for img in files[:8]:
+    im = np.array(Image.open(img).convert('L'))
+    grayimg1.append(im)
+print len(grayimg1)
 
-ims = LFW(min_faces_per_person=20)
-grayfaces = ims.images[:8]
+for img in files[8:]:
+    im = np.array(Image.open(img).convert('L'))
+    grayimg2.append(im)
+print len(grayimg2)
 
-
-
-#Printing the pictures
+#Printing as gray
 pl.gray()
 
-for im in grayfaces:
-	#We use this to sort the images by their mean
-	#np.mean(im.flatten())
 
+#First dataset pictures
+for im in grayimg1:
 	pl.subplot(241+i)
 	i += 1
 	pl.imshow(im)
@@ -38,11 +37,12 @@ for im in grayfaces:
 
 pl.show()
 
-s=0
-print len(grayimg)
-for gim in grayimg:
+#Second dataset images
+print len(grayimg2)
+for gim in grayimg2:
 	pl.subplot(241+s)
 	s+=1
 	pl.imshow(gim)
-	
+	pl.axis('off')
+
 pl.show()
